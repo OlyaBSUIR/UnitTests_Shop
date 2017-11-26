b0bc7502c51ee94cad8a85466f6f188420b27428
OlyaBSUIR <sichnenkoolga@qmail.com>
2017-11-26 12:30:12 +0300

    Created project

---
+++ Check/Check.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyProduct;
+
+namespace MyCheck
+{
+    public class Check
+    {
+        private List<Product> listOfProducts;
+
+        public void addToCheck(Product product)
+        {
+            listOfProducts.Add(product);
+        }
+
+        public int getTotalPrice()
+        {
+            return 0;
+            //var listOfSaleProduct
+        }
+
+    }
+}
---
+++ Product/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProduct
+{
+    public class Product
+    {
+        public String name { get; set; }
+        public int price { get; set; }
+        public bool isOnSale { get; set; }
+        public int countNeedToBuyForSale { get; set; }
+        public int salePrice { get; set; }
+    }
+}
